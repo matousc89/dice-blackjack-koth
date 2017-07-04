7d3f997f8ec57e48d57584719069c3724d3a20c1
Matous C <matous.cejnek@fs.cvut.cz>
2017-07-04 16:39:07 +0200

    player logs

--- controller.py
+++ controller.py
@@ -6,7 +6,7 @@ import random
 
 class Player():
 
-    def __init__(self, source, name, n):
+    def __init__(self, source, name, log, n):
         """
         This class represents player.
         
@@ -16,6 +16,7 @@ class Player():
         self.NAME = name
         self.TARGET = 21
         self.MAX_BET = 5
+        self.LOG = log
         self.SOURCE = source
         self.value = 0
         self.wins = 0
@@ -23,13 +24,14 @@ class Player():
         self.money = n * self.MAX_BET
         self.rolls_count = 1
         
-    def place_bet(self, role, oponent_count):
+    def place_bet(self, role, oponent_log, oponent_count):
         """
         This function contact the bettor to get the demanded bet.
         Bet must be an integer in allowed range.
         """
         args = [
-                "PLACE", self.value, self.rolls_count, oponent_count
+                "PLACE", self.value, self.LOG, oponent_log, self.rolls_count,
+                oponent_count
                 ]
         # testing bot 1
         if self.SOURCE == "TEST1":
@@ -44,10 +46,10 @@ class Player():
         else:
             raise Exception("The place bet get invalid answer.")
 
-    def answer_bet(self, role, oponent_count, oponent_bet):
+    def answer_bet(self, role, oponent_log, oponent_count, oponent_bet):
         args = [
-                "ANSWER", self.value, self.rolls_count, oponent_count,
-                oponent_bet
+                "ANSWER", self.value, self.LOG, oponent_log, self.rolls_count,
+                oponent_count, oponent_bet
                 ]
         # testing bot 1
         if self.SOURCE == "TEST1":
@@ -63,12 +65,12 @@ class Player():
             raise Exception("The answer for bet must be integer 0 or 1.")
 
 
-    def make_decision(self, role):
+    def make_decision(self, role, oponent_log):
         """
         This function obtain decision from AI script or testing function.
         """
         args = [
-            "ROLL", self.value, role
+            "ROLL", self.value, self.LOG, oponent_log, role
             ]
         # testing bot 1
         if self.SOURCE == "TEST1":
@@ -113,7 +115,7 @@ class Player():
         else:
             return self.TARGET - self.value 
         
-    def play_round(self, role):
+    def play_round(self, role, oponent_log):
         """
         This function simulates one round of the player.
         It rolls till AI script call stop.
@@ -125,7 +127,7 @@ class Player():
         # other rolls
         while not done:
             self.penalty = self.estimate_penalty() 
-            decision = self.make_decision(role)
+            decision = self.make_decision(role, oponent_log)
             if not decision:
                 done = True
             else:
@@ -139,20 +141,22 @@ class Player():
         """
         TASK = argv[0]
         VALUE = int(argv[1])
+        MY_LOG = argv[2]
+        OP_LOG = argv[3]
         if TASK == "PLACE":   
-            MY_COUNT = int(argv[2])
-            OP_COUNT = int(argv[3])
+            MY_COUNT = int(argv[4])
+            OP_COUNT = int(argv[5])
             # decide how much bet as bettor  
             return max(1, 21-VALUE)            
         if TASK == "ANSWER":
-            MY_COUNT = int(argv[2])
-            OP_COUNT = int(argv[3])
-            OP_BET = int(argv[4])
+            MY_COUNT = int(argv[4])
+            OP_COUNT = int(argv[5])
+            OP_BET = int(argv[6])
             # decide whether to accept the bet or not
             acceptable = max(1, 21-VALUE) 
             return 1 if acceptable >= OP_BET else 0 
         elif TASK == "ROLL":
-            ROLE = argv[1]   
+            ROLE = argv[4]   
             # decide whether to roll or not     
             if VALUE >= 18:
                 return 0
@@ -165,20 +169,22 @@ class Player():
         """
         TASK = argv[0]
         VALUE = int(argv[1])
+        MY_LOG = argv[2]
+        OP_LOG = argv[3]
         if TASK == "PLACE":   
-            MY_COUNT = int(argv[2])
-            OP_COUNT = int(argv[3])
+            MY_COUNT = int(argv[4])
+            OP_COUNT = int(argv[5])
             # decide how much bet as bettor  
             return max(1, 21-VALUE)            
         if TASK == "ANSWER":
-            MY_COUNT = int(argv[2])
-            OP_COUNT = int(argv[3])
-            OP_BET = int(argv[4])
+            MY_COUNT = int(argv[4])
+            OP_COUNT = int(argv[5])
+            OP_BET = int(argv[6])
             # decide whether to accept the bet or not
             acceptable = max(1, 21-VALUE) 
             return 1 if acceptable >= OP_BET else 0 
         elif TASK == "ROLL":
-            ROLE = argv[1]   
+            ROLE = argv[4]   
             # decide whether to roll or not     
             if VALUE >= 18:
                 return 0
@@ -188,49 +194,68 @@ class Player():
  
 def game_round(dealer, bettor):
     # play
-    dealer.play_round("D")
-    bettor.play_round("B")
+    dealer.play_round("D", bettor.LOG)
+    bettor.play_round("B", dealer.LOG)
     # make bet
-    bet = bettor.place_bet("B", dealer.rolls_count)
-    answer = dealer.answer_bet("D", bettor.rolls_count, bet)
+    bet = bettor.place_bet("B", dealer.LOG, dealer.rolls_count)
+    answer = dealer.answer_bet("D", bettor.LOG, bettor.rolls_count, bet)
     # decide winner
     if answer:
         if bettor.penalty < dealer.penalty:
             # bettor win
             bettor.money += bet
             dealer.money -= bet
-            return "B win"
+            return "B"
         else:
             # dealer win
             bettor.money -= bet
             dealer.money += bet
-            return "D win"
+            return "D"
     else:
         dealer.money -= 1
         bettor.money += 1
-        return "pass"
-        
+        return "P"
+                
 
 
-N = 1000
+N = 10
 #PLAYER1 = 'python3 bots/bot1.py'
 #PLAYER2 = 'python3 bots/bot1.py'
 PLAYER1 = 'TEST1'
 PLAYER2 = 'TEST2'
 
 
-p1 = Player(PLAYER1, "P1", N)
-p2 = Player(PLAYER2, "P2", N)
+p1 = Player(PLAYER1, "P1", "log_p1.log", N)
+p2 = Player(PLAYER2, "P2", "log_p2.log", N)
 
 
+for path in ["log_p1.log", "log_p2.log"]:
+    f = open(path, "w")
+    f.close()
 
 for rnd in range(N):
     if rnd % 2:
-        result = game_round(dealer=p2, bettor=p1)
+        res = game_round(dealer=p2, bettor=p1)
+        linep1 = ["B", str("P" if res == "P" else 1 if res == "B" else 0),
+            str(p1.rolls_count), str(p1.value)]
+        linep2 = ["D", str("P" if res == "P" else 1 if res == "D" else 0),
+            str(p2.rolls_count), str(p2.value)]
     else:    
-        result = game_round(dealer=p1, bettor=p2)
+        res = game_round(dealer=p1, bettor=p2)
+        linep2 = ["B", str("P" if res == "P" else 1 if res == "B" else 0),
+            str(p2.rolls_count), str(p2.value)]
+        linep1 = ["D", str("P" if res == "P" else 1 if res == "D" else 0),
+            str(p1.rolls_count), str(p1.value)]
     
-    print("P"+str(rnd % 2 + 1), "\t", result, "\t", p1.value, p2.value, "\t", "\t", p1.money, p2.money)
+    print("P"+str(rnd % 2 + 1), "\t", res, "\t", p1.value, p2.value, "\t", "\t", p1.money, p2.money)
+    
+    f = open(p1.LOG, "a")
+    f.write("\t".join(linep1)+"\n")
+    f.close()
+    
+    f = open(p2.LOG, "a")
+    f.write("\t".join(linep2)+"\n")
+    f.close()
 
             
 
